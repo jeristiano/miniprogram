53a28cf7aa67a0d3376af72afa3d20a27e9037cd
jeristiano <jeremykuang@foxmail.com>
2017-12-26 22:07:19 +0800

    [增加商品详情]

--- pages/product/product.js
+++ pages/product/product.js
@@ -7,7 +7,9 @@ Page({
    * 页面的初始数据
    */
   data: {
-    id: null
+    id: null,
+    countsArray:[1,2,3,4,5,6,7,8,9,10],
+    productCount:1
   },
 
   /**
@@ -25,9 +27,24 @@ Page({
 
  //定义回调函数接收异步消息
   callbackDetail: function (res) {
+    console.log(res)
     this.setData({
       product: res
     });
   },
+  //选择器绑定事件
+  bindPickerChange:function(event){
+    var index=event.detail.value;
+    var selectCount =this.data.countsArray[index];
+    this.setData({
+      productCount: selectCount
+    });
+  },
+  bindTabItem:function(event){
+    var id = product.getDataSet(event, 'id');
+    this.setData({
+      currentTabIndex: id
+    });
+  }
 
 })
